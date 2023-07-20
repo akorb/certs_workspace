911c19bd15d57756605cbb687982c99d42aa86f8
Andreas Korb <andreas.d.korb@gmail.com>
2023-07-20 10:36:26 +0200

    add comment

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <mbedtls/certs.h>
 
+
+/*
+Goal: Write a program that creates a self-signed certificate (dummy.crt) that is interpretable with
+openssl x509 -in dummy.crt -text
+
+Example code https://github.com/Mbed-TLS/mbedtls/blob/development/programs/x509/cert_write.c
+
+*/
+
 int main()
 {
     return 0;
