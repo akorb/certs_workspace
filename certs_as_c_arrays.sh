2d469637b648a3880c570b75799cac0df3ab2259
Andreas Korb <andreas.d.korb@gmail.com>
2023-07-25 13:28:27 +0200

    Add script to print certs as c arrays

---
+++ certs_as_c_arrays.sh
@@ -0,0 +1,24 @@
+print_ascii_file ()
+{
+    # Arg 1: filename (without extension)
+    # Arg 2: extension
+    # Arg 3: Prefix to array name
+    echo "static const uint8_t $3$1[] ="
+    echo -n '"'
+    head -n -1 $1.$2 | awk '$0=$0"\\n\\"'
+    tail -n 1 $1.$2 | awk '$0=$0"\";"'
+}
+
+print_ascii_file bl1 crt crt_
+echo ''
+
+print_ascii_file bl2 crt crt_
+echo ''
+
+print_ascii_file bl31 crt crt_
+echo ''
+
+print_ascii_file bl32 crt crt_
+echo ''
+
+print_ascii_file bl32 pem key_
