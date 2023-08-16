2ba5d0188052b63a5cbe89517cebeaa74c64176b
Andreas Korb <andreas.d.korb@gmail.com>
2023-08-16 13:07:27 +0200

    Adopt script to new folder structure

--- scripts/certs_as_c_arrays.sh
+++ scripts/certs_as_c_arrays.sh
@@ -1,27 +1,28 @@
 print_ascii_file ()
 {
-    # Arg 1: filename (without extension)
-    # Arg 2: extension
-    # Arg 3: Prefix to array name
-    echo "static const uint8_t $3$1[] ="
+    dir=$1
+    filename=$2  # Without extension
+    extension=$3
+    array_prefix=$4
+    echo "static const uint8_t ${array_prefix}${filename}[] ="
     echo -n '"'
-    head -n -1 $1.$2 | awk '$0=$0"\\n\\"'
-    tail -n 1 $1.$2 | awk '$0=$0"\";"'
+    head -n -1 ${dir}/${filename}.${extension} | awk '$0=$0"\\n\\"'
+    tail -n 1 ${dir}/${filename}.${extension} | awk '$0=$0"\";"'
 }
 
-print_ascii_file certs_out/manufacturer crt crt_
+print_ascii_file certs_out manufacturer crt crt_
 echo ''
 
-print_ascii_file certs_out/bl1 crt crt_
+print_ascii_file certs_out bl1 crt crt_
 echo ''
 
-print_ascii_file certs_out/bl2 crt crt_
+print_ascii_file certs_out bl2 crt crt_
 echo ''
 
-print_ascii_file certs_out/bl31 crt crt_
+print_ascii_file certs_out bl31 crt crt_
 echo ''
 
-print_ascii_file certs_out/bl32 crt crt_
+print_ascii_file certs_out bl32 crt crt_
 echo ''
 
-print_ascii_file keys_in/bl32 pem key_
+print_ascii_file keys_in bl32 pem key_
