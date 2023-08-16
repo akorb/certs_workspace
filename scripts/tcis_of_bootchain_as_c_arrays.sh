2cd8391e74fffbcb4ee777c3ac996b51dc4c5dd9
Andreas Korb <andreas.d.korb@gmail.com>
2023-08-16 13:07:41 +0200

    Do not output bl1 TCI since it's not used anywhere

--- scripts/tcis_of_bootchain_as_c_arrays.sh
+++ scripts/tcis_of_bootchain_as_c_arrays.sh
@@ -20,7 +20,7 @@ OPTEE_ROOT=$1
 echo '#ifndef INCLUDE_TCIs'
 echo '#define INCLUDE_TCIs'
 echo ''
-print_array_of_file_hash ${OPTEE_ROOT}/trusted-firmware-a/build/fvp/release/bl1.bin bl1
+# print_array_of_file_hash ${OPTEE_ROOT}/trusted-firmware-a/build/fvp/release/bl1.bin bl1
 print_array_of_file_hash ${OPTEE_ROOT}/trusted-firmware-a/build/fvp/release/bl2.bin bl2
 print_array_of_file_hash ${OPTEE_ROOT}/trusted-firmware-a/build/fvp/release/bl31.bin bl31
 print_array_of_file_hash ${OPTEE_ROOT}/optee_os/out/arm/core/tee-header_v2.bin bl32
